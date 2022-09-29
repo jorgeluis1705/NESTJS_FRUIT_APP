8ed3f4b6790b2fbee605c8af73f0b673faac56df
Jorge_Luis_Pena_Real <jorgeluispenareal@gmail.com>
2022-09-28 20:40:03 -0500

    main

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
   await app.listen(Number(process.env.PORT) || 3000);
 }
 bootstrap();
